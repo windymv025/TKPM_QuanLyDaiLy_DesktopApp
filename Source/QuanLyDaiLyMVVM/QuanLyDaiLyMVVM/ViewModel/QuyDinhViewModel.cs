daafd9334245f97139b64c6ff485e9d34916b0a6
ThatPham2000 <57900109+ThatPham2000@users.noreply.github.com>
2021-06-28 16:02:37 +0700

    Quy định

--- Source/QuanLyDaiLyMVVM/QuanLyDaiLyMVVM/ViewModel/QuyDinhViewModel.cs
+++ Source/QuanLyDaiLyMVVM/QuanLyDaiLyMVVM/ViewModel/QuyDinhViewModel.cs
@@ -5,6 +5,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace QuanLyDaiLyMVVM.ViewModel
 {
@@ -34,12 +36,80 @@ namespace QuanLyDaiLyMVVM.ViewModel
                 }
             } 
         }
+
+        public ICommand AddCommand { get; set; }
+        public ICommand EditCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+
         public QuyDinhViewModel()
         {
             using (var db = new DBQuanLyCacDaiLyEntities())
             {
-                List = new ObservableCollection<QuyDinh>(db.QuyDinhs);
+                List = new ObservableCollection<QuyDinh>(db.QuyDinhs.Where(x=>x.TrangThai == true).ToList());
             }
+
+            AddCommand = new RelayCommand<QuyDinhWindow>((p) => { 
+                if(!string.IsNullOrEmpty(TenQuyDinh) && !string.IsNullOrEmpty(KieuDuLieu) && !string.IsNullOrEmpty(p.Value.Text.Trim()))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }, (p) => {
+                using (var db = new DBQuanLyCacDaiLyEntities())
+                {
+                    var item = new QuyDinh() { TenQuyDinh = TenQuyDinh, GiaTri = GiaTri, KieuDuLieu = KieuDuLieu, TrangThai = true };
+                    db.QuyDinhs.Add(item);
+                    db.SaveChanges();
+                    List.Add(item);
+                }
+            });
+            EditCommand = new RelayCommand<QuyDinhWindow>((p) => {
+                if (!string.IsNullOrEmpty(TenQuyDinh) && !string.IsNullOrEmpty(KieuDuLieu) && !string.IsNullOrEmpty(p.Value.Text.Trim()) && SelectedItem != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }, (p) => {
+                using (var db = new DBQuanLyCacDaiLyEntities())
+                {
+                    var item = db.QuyDinhs.Where(x => x.Id == SelectedItem.Id).SingleOrDefault();
+                    item.TenQuyDinh = TenQuyDinh;
+                    item.GiaTri = GiaTri;
+                    item.KieuDuLieu = KieuDuLieu;
+                    db.SaveChanges();
+
+                    SelectedItem.TenQuyDinh = TenQuyDinh;
+                    SelectedItem.GiaTri = GiaTri;
+                    SelectedItem.KieuDuLieu = KieuDuLieu;
+                }
+            });
+            DeleteCommand = new RelayCommand<QuyDinhWindow>((p) => { 
+                if(SelectedItem == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }, (p) => {
+                using (var db = new DBQuanLyCacDaiLyEntities())
+                {
+                    var item = db.QuyDinhs.Where(x => x.Id == SelectedItem.Id).SingleOrDefault();
+                    item.TrangThai = false;
+                    db.SaveChanges();
+
+                    SelectedItem.TrangThai = false;
+
+                    List = new ObservableCollection<QuyDinh>(db.QuyDinhs.Where(x => x.TrangThai == true).ToList());
+                }
+            });
         }
     }
 }
