3831b0a68b489e71caee069728f5a63f700e8fb8
ThatPham2000 <57900109+ThatPham2000@users.noreply.github.com>
2021-06-30 12:36:12 +0700

    báo cáo doanh số

---
+++ Source/QuanLyDaiLyMVVM/QuanLyDaiLyMVVM/Model/DoanhSo.cs
@@ -0,0 +1,21 @@
+﻿using QuanLyDaiLyMVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyDaiLyMVVM.Model
+{
+    public class DoanhSo : BaseViewModel
+    {
+        private int _IdDaiLy;
+        public int IdDaiLy { get => _IdDaiLy; set { _IdDaiLy = value; OnPropertyChanged(); } }
+        private string _TenDaiLy;
+        public string TenDaiLy { get => _TenDaiLy; set { _TenDaiLy = value; OnPropertyChanged(); } }
+        private decimal _TongTien;
+        public decimal TongTien { get => _TongTien; set { _TongTien = value; OnPropertyChanged(); } }
+        private double _Tyle;
+        public double Tyle { get => _Tyle; set { _Tyle = value; OnPropertyChanged(); } }
+    }
+}
