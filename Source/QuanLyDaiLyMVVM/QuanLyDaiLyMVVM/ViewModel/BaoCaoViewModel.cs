75fce6bf576f359e2e63cb8060cd6c9389376240
ThatPham2000 <57900109+ThatPham2000@users.noreply.github.com>
2021-07-05 22:36:27 +0700

    export excel báo cáo công nợ

--- Source/QuanLyDaiLyMVVM/QuanLyDaiLyMVVM/ViewModel/BaoCaoViewModel.cs
+++ Source/QuanLyDaiLyMVVM/QuanLyDaiLyMVVM/ViewModel/BaoCaoViewModel.cs
@@ -113,7 +113,7 @@ namespace QuanLyDaiLyMVVM.ViewModel
                         ws.Cells.Style.Font.Name = "Times New Roman";
 
                         // Tạo danh sách các column header
-                        string[] arrColumnHeader = {"Tên doanh nghiệp", "Tổng tiền bán được", "Tỷ lệ"};
+                        string[] arrColumnHeader = {"Tên đại lý", "Tổng tiền bán được", "Tỷ lệ"};
 
                         // lấy ra số lượng cột cần dùng dựa vào số lượng header
                         var countColHeader = arrColumnHeader.Count();
@@ -176,8 +176,13 @@ namespace QuanLyDaiLyMVVM.ViewModel
 
                         }
                         ws.Cells[++rowIndex, 1].Value = "Tổng doanh số bán hàng";
+                        ws.Cells[rowIndex, 1].Style.Font.Bold = true;
+
                         ws.Cells[rowIndex, 2].Value = TongDoanhSo;
+                        ws.Cells[rowIndex, 2].Style.Font.Bold = true;
+
                         ws.Cells[rowIndex, 3].Value = "100%";
+                        ws.Cells[rowIndex, 3].Style.Font.Bold = true;
 
                         //Lưu file lại
                         Byte[] bin = p.GetAsByteArray();
@@ -185,14 +190,139 @@ namespace QuanLyDaiLyMVVM.ViewModel
                     }
                     MessageBox.Show("Xuất excel thành công!");
                 }
-                catch (Exception e)
+                catch
                 {
-                    MessageBox.Show(e.Message);
+                    MessageBox.Show("Có lỗi khi lưu file!");
                 }
             });
 
-            ExportCongNoCommand = new RelayCommand<QuyDinhWindow>((p) => { return true; }, (p) => {
-                
+            ExportCongNoCommand = new RelayCommand<QuyDinhWindow>((q) => { return true; }, (q) => {
+                string filePath = "";
+                // tạo SaveFileDialog để lưu file excel
+                SaveFileDialog dialog = new SaveFileDialog();
+
+                // chỉ lọc ra các file có định dạng Excel
+                dialog.Filter = "Excel | *.xlsx | Excel 2003 | *.xls";
+
+                // Nếu mở file và chọn nơi lưu file thành công sẽ lưu đường dẫn lại dùng
+                if (dialog.ShowDialog() == true)
+                {
+                    filePath = dialog.FileName;
+                }
+
+                // nếu đường dẫn null hoặc rỗng thì báo không hợp lệ và return hàm
+                if (string.IsNullOrEmpty(filePath))
+                {
+                    MessageBox.Show("Đường dẫn báo cáo không hợp lệ");
+                    return;
+                }
+
+                try
+                {
+                    // If you use EPPlus in a noncommercial context
+                    // according to the Polyform Noncommercial license:
+                    ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+                    using (ExcelPackage p = new ExcelPackage())
+                    {
+                        // đặt tên người tạo file
+                        p.Workbook.Properties.Author = "Pham Van That";
+
+                        // đặt tiêu đề cho file
+                        p.Workbook.Properties.Title = "Báo cáo thống kê";
+
+                        //Tạo một sheet để làm việc trên đó
+                        p.Workbook.Worksheets.Add("Công nợ");
+
+                        // lấy sheet vừa add ra để thao tác
+                        ExcelWorksheet ws = p.Workbook.Worksheets[0];
+
+                        // đặt tên cho sheet
+                        ws.Name = "Công nợ";
+                        // fontsize mặc định cho cả sheet
+                        ws.Cells.Style.Font.Size = 12;
+                        // font family mặc định cho cả sheet
+                        //ws.Cells.Style.Font.Name = "Calibri";
+                        ws.Cells.Style.Font.Name = "Times New Roman";
+
+                        // Tạo danh sách các column header
+                        string[] arrColumnHeader = { "Tên đại lý", "Tổng tiền nợ" };
+
+                        // lấy ra số lượng cột cần dùng dựa vào số lượng header
+                        var countColHeader = arrColumnHeader.Count();
+
+                        // merge các column lại từ column 1 đến số column header
+                        // gán giá trị cho cell vừa merge là Thống kê thông tni User Kteam
+                        ws.Cells[1, 1].Value = $"Thống kê công nợ";
+                        ws.Cells[1, 1, 1, countColHeader].Merge = true;
+                        // in đậm
+                        ws.Cells[1, 1, 1, countColHeader].Style.Font.Bold = true;
+                        // căn giữa
+                        ws.Cells[1, 1, 1, countColHeader].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+                        int colIndex = 1;
+                        int rowIndex = 2;
+
+                        //tạo các header từ column header đã tạo từ bên trên
+                        foreach (var item in arrColumnHeader)
+                        {
+                            var cell = ws.Cells[rowIndex, colIndex];
+
+                            //set màu thành gray
+                            var fill = cell.Style.Fill;
+                            fill.PatternType = ExcelFillStyle.Solid;
+                            fill.BackgroundColor.SetColor(System.Drawing.Color.LightBlue);
+
+                            //căn chỉnh các border
+                            var border = cell.Style.Border;
+                            border.Bottom.Style =
+                                border.Top.Style =
+                                border.Left.Style =
+                                border.Right.Style = ExcelBorderStyle.Thin;
+                            cell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+                            //gán giá trị
+                            cell.Value = item;
+
+                            colIndex++;
+                        }
+
+                        // lấy ra danh sách UserInfo từ ItemSource của DataGrid
+                        //List<UserInfo> userList = dtgExcel.ItemsSource.Cast<UserInfo>().ToList();
+
+                        // với mỗi item trong danh sách sẽ ghi trên 1 dòng
+                        foreach (var item in CongNoHienThi)
+                        {
+                            // bắt đầu ghi từ cột 1. Excel bắt đầu từ 1 không phải từ 0
+                            colIndex = 1;
+
+                            // rowIndex tương ứng từng dòng dữ liệu
+                            rowIndex++;
+
+                            //gán giá trị cho từng cell                      
+                            ws.Cells[rowIndex, colIndex++].Value = item.TenDaiLy;
+                            ws.Cells[rowIndex, colIndex++].Value = item.TienNo;
+
+                            // lưu ý phải .ToShortDateString để dữ liệu khi in ra Excel là ngày như ta vẫn thấy.Nếu không sẽ ra tổng số :v
+                            //ws.Cells[rowIndex, colIndex++].Value = item.Birthday.ToShortDateString();
+
+                        }
+                        ws.Cells[++rowIndex, 1].Value = "Tổng tiền nợ";
+                        ws.Cells[rowIndex, 1].Style.Font.Bold = true;
+
+                        ws.Cells[rowIndex, 2].Value = TongCongNo;
+                        ws.Cells[rowIndex, 2].Style.Font.Bold = true;
+
+                        //Lưu file lại
+                        Byte[] bin = p.GetAsByteArray();
+                        File.WriteAllBytes(filePath, bin);
+                    }
+                    MessageBox.Show("Xuất excel thành công!");
+                }
+                catch
+                {
+                    MessageBox.Show("Có lỗi khi lưu file!");
+                }
             });
         }
 
